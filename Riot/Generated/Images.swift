d9d2e0086580e5dc40dc3c1e53974271604e0d87
Marco Festini <marco.festini@awesome-technologies.de>
2019-05-13 12:37:27 +0200

    Restructure assets to allow share extension to use some of them
    - Add Caritas assets to share extension target
    - Create new SharedImages asset with e2e and cancel icons
    - Add SharedImages.xcassets to swiftgen-config to reflect changes in structure
    
    Change-Id: Ib5149167c51cdc930d6ef9dc209766b54e2a02d1

--- Riot/Generated/Images.swift
+++ Riot/Generated/Images.swift
@@ -34,7 +34,6 @@ internal enum Asset {
     internal static let animatedDots4 = ImageAsset(name: "animatedDots-4")
     internal static let adminIcon = ImageAsset(name: "admin_icon")
     internal static let backIcon = ImageAsset(name: "back_icon")
-    internal static let cancel = ImageAsset(name: "cancel")
     internal static let chevron = ImageAsset(name: "chevron")
     internal static let disclosureIcon = ImageAsset(name: "disclosure_icon")
     internal static let group = ImageAsset(name: "group")
@@ -47,7 +46,6 @@ internal enum Asset {
     internal static let startChat = ImageAsset(name: "start_chat")
     internal static let e2eBlocked = ImageAsset(name: "e2e_blocked")
     internal static let e2eUnencrypted = ImageAsset(name: "e2e_unencrypted")
-    internal static let e2eVerified = ImageAsset(name: "e2e_verified")
     internal static let e2eWarning = ImageAsset(name: "e2e_warning")
     internal static let leave = ImageAsset(name: "leave")
     internal static let notifications = ImageAsset(name: "notifications")
@@ -84,6 +82,10 @@ internal enum Asset {
     internal static let tabPeople = ImageAsset(name: "tab_people")
     internal static let tabRooms = ImageAsset(name: "tab_rooms")
     internal static let tabSettings = ImageAsset(name: "tab_settings")
+  }
+  internal enum SharedImages {
+    internal static let cancel = ImageAsset(name: "cancel")
+    internal static let e2eVerified = ImageAsset(name: "e2e_verified")
   }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
