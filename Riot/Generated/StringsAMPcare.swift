35f4041fe4a3e9fb18876f3d203630e22dd15a36
Marco Festini <marco.festini@awesome-technologies.de>
2019-09-26 12:36:26 +0200

    Add AMPcare l10n string generation
    Change-Id: I319bc8169ac39869edfa649bd1c479ed61c3800f

---
+++ Riot/Generated/StringsAMPcare.swift
@@ -0,0 +1,310 @@
+// swiftlint:disable all
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Strings
+
+// swiftlint:disable function_parameter_count identifier_name line_length type_body_length
+internal enum AMPcareL10n {
+  /// Anamnesis
+  internal static var anamnesis: String { 
+    return AMPcareL10n.tr("AMPcare", "anamnesis") 
+  }
+  /// Blood Pressure
+  internal static var bloodPressure: String { 
+    return AMPcareL10n.tr("AMPcare", "blood-pressure") 
+  }
+  /// Body Temperature
+  internal static var bodyTemperature: String { 
+    return AMPcareL10n.tr("AMPcare", "body-temperature") 
+  }
+  /// Body Weight
+  internal static var bodyWeight: String { 
+    return AMPcareL10n.tr("AMPcare", "body-weight") 
+  }
+  /// Anamnesis
+  internal static var caseDetailSectionAnamnesis: String { 
+    return AMPcareL10n.tr("AMPcare", "case_detail_section_anamnesis") 
+  }
+  /// Data
+  internal static var caseDetailSectionData: String { 
+    return AMPcareL10n.tr("AMPcare", "case_detail_section_data") 
+  }
+  /// Heart
+  internal static var caseDetailSectionHeart: String { 
+    return AMPcareL10n.tr("AMPcare", "case_detail_section_heart") 
+  }
+  /// Message
+  internal static var caseDetailSectionMessage: String { 
+    return AMPcareL10n.tr("AMPcare", "case_detail_section_message") 
+  }
+  /// Vitals
+  internal static var caseDetailSectionVitals: String { 
+    return AMPcareL10n.tr("AMPcare", "case_detail_section_vitals") 
+  }
+  /// Case
+  internal static var caseDetailTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "case_detail_title") 
+  }
+  /// Cases
+  internal static var caseListTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "case_list_title") 
+  }
+  /// Preview
+  internal static var casePreviewTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "case_preview_title") 
+  }
+  /// Critical
+  internal static var caseSeverityCritical: String { 
+    return AMPcareL10n.tr("AMPcare", "case_severity_critical") 
+  }
+  /// Info
+  internal static var caseSeverityInfo: String { 
+    return AMPcareL10n.tr("AMPcare", "case_severity_info") 
+  }
+  /// Request
+  internal static var caseSeverityRequest: String { 
+    return AMPcareL10n.tr("AMPcare", "case_severity_request") 
+  }
+  /// Urgent
+  internal static var caseSeverityUrgent: String { 
+    return AMPcareL10n.tr("AMPcare", "case_severity_urgent") 
+  }
+  /// Created
+  internal static var created: String { 
+    return AMPcareL10n.tr("AMPcare", "created") 
+  }
+  /// Diastolic…
+  internal static var diastolicPlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "diastolic_placeholder") 
+  }
+  /// Added pictures
+  internal static var editCaseAddPictures: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_add_pictures") 
+  }
+  /// Anamnesis
+  internal static var editCaseAnamnesisTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_anamnesis_title") 
+  }
+  /// Blood Pressure
+  internal static var editCaseBloodPressureTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_blood-pressure_title") 
+  }
+  /// Body Temperature
+  internal static var editCaseBodyTemperatureTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_body-temperature_title") 
+  }
+  /// Body Weight
+  internal static var editCaseBodyWeightTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_body-weight_title") 
+  }
+  /// Edit Case
+  internal static var editCaseButton: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_button") 
+  }
+  /// Glucose
+  internal static var editCaseGlucoseTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_glucose_title") 
+  }
+  /// Heart Rate
+  internal static var editCaseHeartRateTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_heart-rate_title") 
+  }
+  /// Last Defecation
+  internal static var editCaseLastDefecationTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_last-defecation_title") 
+  }
+  /// Misc…
+  internal static var editCaseMiscPlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_misc_placeholder") 
+  }
+  /// Misc
+  internal static var editCaseMiscTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_misc_title") 
+  }
+  /// Note…
+  internal static var editCaseNotePlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_note_placeholder") 
+  }
+  /// Note
+  internal static var editCaseNoteTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_note_title") 
+  }
+  /// Pain…
+  internal static var editCasePainPlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_pain_placeholder") 
+  }
+  /// Pain
+  internal static var editCasePainTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_pain_title") 
+  }
+  /// Patient name…
+  internal static var editCasePatientPlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_patient_placeholder") 
+  }
+  /// Patient
+  internal static var editCasePatientTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_patient_title") 
+  }
+  /// Pictures
+  internal static var editCasePicturesTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_pictures_title") 
+  }
+  /// Recipient
+  internal static var editCaseRecipientTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_recipient_title") 
+  }
+  /// Requester
+  internal static var editCaseRequesterTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_requester_title") 
+  }
+  /// Responsiveness…
+  internal static var editCaseResponsivenessPlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_responsiveness_placeholder") 
+  }
+  /// Responsiveness
+  internal static var editCaseResponsivenessTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_responsiveness_title") 
+  }
+  /// Severity
+  internal static var editCaseSeverityTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_severity_title") 
+  }
+  /// Edit Case
+  internal static var editCaseTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_title") 
+  }
+  /// Title…
+  internal static var editCaseTitlePlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_title_placeholder") 
+  }
+  /// Title
+  internal static var editCaseTitleTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_title_title") 
+  }
+  /// Vitals
+  internal static var editCaseVitalsTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "edit_case_vitals_title") 
+  }
+  /// Glucose
+  internal static var glucose: String { 
+    return AMPcareL10n.tr("AMPcare", "glucose") 
+  }
+  /// Heart Rate
+  internal static var heartRate: String { 
+    return AMPcareL10n.tr("AMPcare", "heart-rate") 
+  }
+  /// Last Defecation
+  internal static var lastDefecation: String { 
+    return AMPcareL10n.tr("AMPcare", "last-defecation") 
+  }
+  /// Misc
+  internal static var misc: String { 
+    return AMPcareL10n.tr("AMPcare", "misc") 
+  }
+  /// New Case
+  internal static var newCaseButton: String { 
+    return AMPcareL10n.tr("AMPcare", "new_case_button") 
+  }
+  /// New Case
+  internal static var newCaseTitle: String { 
+    return AMPcareL10n.tr("AMPcare", "new_case_title") 
+  }
+  /// Note
+  internal static var note: String { 
+    return AMPcareL10n.tr("AMPcare", "note") 
+  }
+  /// Pain
+  internal static var pain: String { 
+    return AMPcareL10n.tr("AMPcare", "pain") 
+  }
+  /// Patient
+  internal static var patient: String { 
+    return AMPcareL10n.tr("AMPcare", "patient") 
+  }
+  /// Birthdate
+  internal static var patientEditBirthDateLabel: String { 
+    return AMPcareL10n.tr("AMPcare", "patient_edit_birthDate_label") 
+  }
+  /// Gender
+  internal static var patientEditGenderLabel: String { 
+    return AMPcareL10n.tr("AMPcare", "patient_edit_gender_label") 
+  }
+  /// Female
+  internal static var patientGenderFemale: String { 
+    return AMPcareL10n.tr("AMPcare", "patient_gender_female") 
+  }
+  /// Male
+  internal static var patientGenderMale: String { 
+    return AMPcareL10n.tr("AMPcare", "patient_gender_male") 
+  }
+  /// Other
+  internal static var patientGenderOther: String { 
+    return AMPcareL10n.tr("AMPcare", "patient_gender_other") 
+  }
+  /// Unknown
+  internal static var patientGenderUnknown: String { 
+    return AMPcareL10n.tr("AMPcare", "patient_gender_unknown") 
+  }
+  /// Pictures
+  internal static var pictures: String { 
+    return AMPcareL10n.tr("AMPcare", "pictures") 
+  }
+  /// Recipient
+  internal static var recipient: String { 
+    return AMPcareL10n.tr("AMPcare", "recipient") 
+  }
+  /// Requester
+  internal static var requester: String { 
+    return AMPcareL10n.tr("AMPcare", "requester") 
+  }
+  /// Responsiveness
+  internal static var responsiveness: String { 
+    return AMPcareL10n.tr("AMPcare", "responsiveness") 
+  }
+  /// Severity
+  internal static var severity: String { 
+    return AMPcareL10n.tr("AMPcare", "severity") 
+  }
+  /// Systolic…
+  internal static var systolicPlaceholder: String { 
+    return AMPcareL10n.tr("AMPcare", "systolic_placeholder") 
+  }
+  /// Title
+  internal static var title: String { 
+    return AMPcareL10n.tr("AMPcare", "title") 
+  }
+  /// Vitals
+  internal static var vitals: String { 
+    return AMPcareL10n.tr("AMPcare", "vitals") 
+  }
+  /// Years
+  internal static var years: String { 
+    return AMPcareL10n.tr("AMPcare", "years") 
+  }
+}
+// swiftlint:enable function_parameter_count identifier_name line_length type_body_length
+
+// MARK: - Implementation Details
+
+extension AMPcareL10n {
+  static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    let locale: Locale
+    if let localeIdentifier = Bundle.mxk_language() {
+       locale = Locale(identifier: localeIdentifier)
+    } else if let fallbackLocaleIdentifier = Bundle.mxk_fallbackLanguage() {
+       locale = Locale(identifier: fallbackLocaleIdentifier)
+    } else {
+       locale = Locale.current
+    }        
+
+      return String(format: format, locale: locale, arguments: args)
+    }
+}
+
+private final class BundleToken {}
