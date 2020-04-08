c18185e44ad3ac9b27c34bab769eac6954e57d48
Marco Festini <marco.festini@awesome-technologies.de>
2020-04-08 20:50:14 +0200

    Automatically join room when they get added to the cases list
    This change causes the cases list to be sorted correctly
    
    Change-Id: Id61f0a863602844a02b408075b7ac3ddefdce815

--- Riot/Modules/AMP.care/Controllers/Cases List/CasesDataSource.swift
+++ Riot/Modules/AMP.care/Controllers/Cases List/CasesDataSource.swift
@@ -12,4 +12,25 @@ class CasesDataSource: MXKRecentsDataSource {
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return false
     }
+    
+    // Automatically join any room the user is invited to
+    override func dataSource(_ dataSource: MXKDataSource!, didCellChange changes: Any!) {
+        if let recentsDataSourceArray = value(forKey: "displayedRecentsDataSourceArray") as? [MXKSessionRecentsDataSource], recentsDataSourceArray.count > 0 {
+            
+            let recentsDataSource = recentsDataSourceArray[0]
+            let numberOfCells = recentsDataSource.numberOfCells
+            for cellIndex in 0..<numberOfCells {
+                let cell = recentsDataSource.cellData(at: cellIndex)
+                if cell?.roomSummary.membership == .invite {
+                    cell?.roomSummary.room.join(completion: { response in
+                        if response.isFailure, let error = response.error {
+                            print("CasesDataSource: init - Error joining room %@", error.localizedDescription)
+                        }
+                    })
+                }
+            }
+        }
+        
+        super.dataSource(dataSource, didCellChange: changes)
+    }
 }
